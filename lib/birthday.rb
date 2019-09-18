824e8c00a2ffb19fc400770a2dc5abdb8688cec7
Blythe Pollard <blytheepollard@gmail.com>
2019-09-18 21:38:23 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -13,7 +13,9 @@ end
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-    if age > 12 
+    if age < 12
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    elsif age > 12 
       puts "Happy Birthday #{kids_name}! You are now #{age} years old! You are too old for this."
     end  
     end
